1e5850e6196863c31080e48e5ec2cfed9f6fcda9
xusun000 <2235861811@qq.com>
2025-10-07 11:20:25 +0800

    update 2025-10-07 11:20

--- push.sh
+++ push.sh
@@ -1,3 +1,6 @@
-git add .
-git commit -m "update $(date +"%Y-%m-%d %H:%M")"
-git push
+while true; do
+    git add .
+    git commit -m "update $(date +"%Y-%m-%d %H:%M")"
+    git push
+    sleep 30
+done
