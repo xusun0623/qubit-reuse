3a3d2755057c628bfc6e299ee45cc97b31fa5f9d
xusun000 <2235861811@qq.com>
2025-10-07 11:46:11 +0800

    update 2025-10-07 11:46

--- qrmap_compiler.py
+++ qrmap_compiler.py
@@ -43,6 +43,7 @@ class QRMapCompiler:
         # 5. 最小化横向宽度
         # Qubit Reuse会有一个, (q_3 -> q_2)[g_2], (q_4 -> q_2)[g_1]
         # 红线：[g_0, g_1, q_4, q_4], 假设移动q_4到q_2, 则表示为 [g_0, g_1, q_2, q_4]（原始的在q_4上）
+        # another：[g_0, g_4, q_0, q_0]
 
     def export_matrix_to_csv(self, mat, filename="./output/qubit_matrix.csv"):
         # 导出矩阵到CSV文件，用于可视化和调试
