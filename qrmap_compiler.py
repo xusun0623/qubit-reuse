c31953894dc7fdb5a53f70a80a89e82295830830
xusun000 <2235861811@qq.com>
2025-10-07 11:44:06 +0800

    update 2025-10-07 11:44

--- qrmap_compiler.py
+++ qrmap_compiler.py
@@ -44,7 +44,6 @@ class QRMapCompiler:
         # Qubit Reuse会有一个, (q_3 -> q_2)[g_2], (q_4 -> q_2)[g_1]
         # 红线：[g_0, g_1, q_4], 假设移动q_4到q_2, 则表示为 [g_0, g_1, q_2]
         
-        
 
     def export_matrix_to_csv(self, mat, filename="./output/qubit_matrix.csv"):
         # 导出矩阵到CSV文件，用于可视化和调试
